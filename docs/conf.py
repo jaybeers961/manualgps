072bcde55680dc42e18132360b57112702f91cdf
jaybeers961 <jaybeers961@gmail.com>
2026-02-14 15:42:00 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,15 +1,28 @@
-project = "Garmin GPS"
-author = 'Your Name'
+project = 'garmin'
+author = 'garmin'
 release = '1.0'
 
-extensions = []
+# Extensions
+extensions = [
+    'sphinx_sitemap',
+]
+
+# Paths
 templates_path = ['_templates']
 exclude_patterns = []
 
+# Theme
 html_theme = 'alabaster'
 html_static_path = ['_static']
 
-html_js_files = [
-    'chat.js',
-]
-html_favicon = '_static/favion.png'
+# Custom JS & Favicon
+html_js_files = ['chatbot.js']  # chatbot widget
+html_favicon = '_static/favicon.png'
+
+# Bing search verification
+html_context = {
+    'bing_verification_code': '89E119164B044B0B4EE80A50AB60418E'
+}
+
+# Base URL for sitemap
+html_baseurl = 'https://garmingpshelp.readthedocs.io/en/latest/'
